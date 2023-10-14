0dec64937d60fe76f58b1577f3f39b666297ce67
PennyS8 <silliman25@up.edu>
2023-10-14 12:43:23 -0700

    Silliman__added newVariable() with docstring comment

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ def main():
     # call eliminate_terminal_non-terminal()
     # call eliminate_non-terminal_groups()
     # call export_JSON()
+
+
+
+def newVariable(variables):
+    """
+    Create a new unique variable and append it to the variables list.
+    Note: This function currently finds a variable of the form "Xn" where n
+    is an integer greater than or equal to zero.
+    TODO: change this function to create a unique single character symbol
+    at least up to the whole 26 letter alphabet.
+    """
+    var = "X"
+    i = 0
+    while (var + i) in variables:
+        i+=1
+    variables.append(var + i)
