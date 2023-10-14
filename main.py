be4fb8f73bfc9f9ba4580a942d79bc2ecaa0c511
PennyS8 <silliman25@up.edu>
2023-10-14 12:31:52 -0700

    Silliman__reformed main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-variables = [];
-
-def newVariable(variables):
-    var = "X"
-    i = 0
-    while (var + i) in variables:
-        i+=1
-    variables.append(var + i)
-
-
-
-
+def main():
+    pass
+    # define CFL data structure
+    # call import_JSON()
+    # call new_start_rule()
+    # call eliminate_useless_rules)()
+    # call eliminate_epsilons()
+    # call eliminate_unit_productions)()
+    # call eliminate_terminal_non-terminal()
+    # call eliminate_non-terminal_groups()
+    # call export_JSON()
