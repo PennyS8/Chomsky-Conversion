e49d36647dd5ecf085a2b160df27dccf9964a66c
PennyS8 <silliman25@up.edu>
2023-10-18 01:20:52 -0700

    Silliman__completed testing framework

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,21 +1,17 @@
 import pytest
 
-from src import main
 from src.main import *
 
-
-# fixture is not equal to cfl object, both test_conversion2 and test_conversion1 should pass
 @pytest.fixture()
 def example_CFG():
-    return main.CFL(["V", "W", "S"],
-                    ["a", "b"],
-                    [   {"LHS": "S", "RHS": ["_epsilon_", "V", "aSb"]},
-		                {"LHS": "V", "RHS": ["aW", "Wb"]},
-		                {"LHS": "W", "RHS": ["_epsilon_", "a", "b"]}],
-                    "S")
+    return CFL(["V", "W", "S"],
+                ["a", "b"],
+                [   {"LHS": "S", "RHS": ["_epsilon_", "V", "aSb"]},
+		            {"LHS": "V", "RHS": ["aW", "Wb"]},
+		            {"LHS": "W", "RHS": ["_epsilon_", "a", "b"]}],
+                "S")
 
-
-def test_conversion1():
+def test_import_JSON():
     input_file_path = "input_CFLs/example.json"
     cfl = import_JSON(input_file_path)
     assert cfl.variables == ["V", "W", "S"]
@@ -25,7 +21,15 @@ def test_conversion1():
 		                            {"LHS": "W", "RHS": ["_epsilon_", "a", "b"]}]
     assert cfl.start_state == "S"
 
-def test_conversion2():
-    input_file_path = "input_CFLs/example.json"
-    cfl = import_JSON(input_file_path)
-    assert cfl == example_CFG
+def test_export_JSON(example_CFG):
+    output_file_path = "output_CFLs/example.json"
+    export_JSON(example_CFG, output_file_path)
+    # since we already proved that import_JSON works we can use it to
+    # prove export_JSON works
+    cfl = import_JSON(output_file_path)
+    assert cfl.variables == ["V", "W", "S"]
+    assert cfl.terminals == ["a", "b"]
+    assert cfl.production_rules == [{"LHS": "S", "RHS": ["_epsilon_", "V", "aSb"]},
+		                            {"LHS": "V", "RHS": ["aW", "Wb"]},
+		                            {"LHS": "W", "RHS": ["_epsilon_", "a", "b"]}]
+    assert cfl.start_state == "S"
