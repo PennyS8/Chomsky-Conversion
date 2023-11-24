ac6e545ed568f64e55f71ac704ed2695196108cf
PennyS8 <silliman25@up.edu>
2023-11-24 01:31:15 -0800

    Silliman__optimized but bug w/ duplicate et_var's

--- src/main.py
+++ src/main.py
@@ -185,25 +185,29 @@ def eliminate_epsilons(cfl):
     Note: that if the grammar accepts the empty string there must be an epsilon in the
     start state's production rule.
     """
-    e_list = [] # list of variables that terminate to epsilon
+    e_set = set() # set of variables that terminate to epsilon
+    
+    # find variables that terminate to epsilon
     for rule in cfl.production_rules:
         if rule["LHS"] != cfl.start_state: # exception is made for the start state
             for i, product in enumerate(rule["RHS"]):
                 if "_epsilon_" in product:
                     # append the variable to e_list to substitute it later
-                    e_list.append(rule["LHS"])
+                    e_set.add(rule["LHS"])
                     rule["RHS"].pop(i) # remove item from the product list
+
+    e_list = list(e_set)
 
     # if a rule has a production with a variable in e_list, then duplicate that
     # production excluding the epsilon terminating variable
     for rule in cfl.production_rules:
         for product in rule["RHS"]: # for each possible product of this rule...
             # does this product contain a variable that would terminate to epsilon
-            for child_var in e_list:
-                if str(child_var) in product:
+            for et_var in e_list: # et_var (Epsilon Terminating VARiable)
+                if et_var in product:
                     # duplicate the product excluding the var in child_var
-                    new_product = re.sub(child_var, "", product, count=1)
-                    if len(new_product) != 0:
+                    new_product = re.sub(et_var, "", product, count=1)
+                    if len(new_product) != 0 and new_product not in rule["RHS"]:
                         rule["RHS"].append(new_product)
 
 
