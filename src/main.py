22634903b16297b09f436f9e316dfedb847cfcbe
mnguytwofive <nguy25@up.edu>
2023-11-26 18:45:07 -0800

    Eliminate Useless Rules Function
    The function removes:
    1) all unreachable transitions
    2) all production rules whose variable isn't part of variable list
    3) creates epsilon transition if production rule contains empty RHS (test case)

--- src/main.py
+++ src/main.py
@@ -56,12 +56,13 @@ def main(current_path):
                 print("    " + rule["LHS"] + " -> " + ", ".join(rule["RHS"]))
             print("  Start Variable: " + cfl.start_var)
 
-            #add_new_start_rule(cfl)
-            #remove_useless_rules(cfl)
+            check_proper_form(cfl)
+            add_new_start_rule(cfl)
+            remove_useless_rules(cfl)
             #remove_epsilons(cfl)
             #remove_unit_products(cfl)
             #isolate_terminals(cfl)
-            remove_variable_groups(cfl)
+            #remove_nonterminal_groups(cfl)
 
             # TODO: remove print()'s once program is finished
             print("\noutput_CFL: ")
